50d85397d0173ab92552f0a23dc43ddefca43b82
matteobnvl <matteobonneval19@gmail.com>
2022-10-11 18:02:05 +0200

    chat/appel

--- js/app.js
+++ js/app.js
@@ -1,23 +1,3 @@
-
-
-function ActiveGroup(){
-  var group = document.getElementById("Group");
-  var chat = document.getElementById("chat");
-  var btnGroup = document.getElementById("btn-group");
-  var btnClose = document.getElementById("btn-close");
-
-  group.classList.toggle("active-block");
-  chat.classList.toggle("active-none");
-  btnGroup.classList.toggle("active-none");
-  btnClose.classList.toggle("active-block");
-
-}
-
-
-
-
-
-
 $(function () {
     var includes = $('[data-include]')
     $.each(includes, function () {
